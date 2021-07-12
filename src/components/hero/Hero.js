07db4a940033ff7b0d3bf230449d768ef3a5c0db
Tiago da Silva Valeriano de Oliveira <tiago.oliveira@ifb.edu.br>
2021-07-12 18:06:50 -0300

    Adjustment of hero component.

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,33 +1,33 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, Carousel } from 'react-bootstrap'
 import slide1 from '../../assets/imgs/20200331_111944.jpg'
+import mainCarousselItems from '../../fixtures/mainCarousselItems.js'
 import './Hero.css'
 
 export default class CNHero extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            carouselItems: mainCarousselItems,
+        }
+    }
+
     render() {
         return (<header id="hero" className="cn-hero">        
         <Container>
           <Row>
             <Col>
             <Carousel>
-              <Carousel.Item style={{backgroundImage: 'url(' + slide1 + ')'}}>
-                <Carousel.Caption>
-                  <h3>First slide label</h3>
-                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item style={{backgroundImage: 'url(' + slide1 + ')'}}>
-                <Carousel.Caption>
-                  <h3>Second slide label</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item style={{backgroundImage: 'url(' + slide1 + ')'}}>
-                <Carousel.Caption>
-                  <h3>Third slide label</h3>
-                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
+              {this.state.carouselItems.map((item) => {
+                return (
+                  <Carousel.Item style={{backgroundImage: 'url(' + item.image.default + ')'}}>
+                    <Carousel.Caption>
+                      <h3>{item.title}</h3>
+                      <p>{item.label}</p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                )
+              })}
             </Carousel>
             </Col>
           </Row>
