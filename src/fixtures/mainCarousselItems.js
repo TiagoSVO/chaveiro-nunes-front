a0866e84492092f159e3fec034d96e80c303dcd0
Tiago da Silva Valeriano de Oliveira <tiago.oliveira@ifb.edu.br>
2021-08-13 10:23:05 -0300

    Updates

--- src/fixtures/mainCarousselItems.js
+++
@@ -1,6 +0,0 @@
-module.exports = [{
-  "id":1,
-  "image": require("../assets/imgs/20200331_112020.jpg"),
-  "title": "Chaveiro Nunes",
-  "label": "Serviços gerais de chaveiro exercido com compromisso e profissionalidade."
-}]
